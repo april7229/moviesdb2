1a4917df6a19aed9083e4efd6d554cce1a2b34ca
april <martinezapril56@yahoo.com>
2019-01-19 13:01:36 -0500

    tryign to make it work

--- movies_app/src/components/SearchBar/SearchBar.js
+++ movies_app/src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-Searimport React, { Component } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './SearchBar.css';
